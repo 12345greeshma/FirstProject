67288e3be753873e52b343ea35df641cd74387bc
12345greeshma <1990greeshma@gmail.com>
2016-01-07 10:46:01 +0530

    third commit

--- First.java
+++ First.java
@@ -4,8 +4,18 @@ package com.trivand;
  * Created by trivand on 7/1/16.
  */
 public class First {
+    public void addi() {
+        int a = 3, b = 6, c;
+        c = a + b;
+        System.out.println("Sum of 6 + 3 is :"+c);
+    }
+
     public static void main(String args[]){
+        First f = new First();
+        f.addi();
         System.out.println("hi");
         System.out.println("greeshma");
+
+
     }
 }
